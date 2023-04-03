49088891cd8f83c527fd836a1582bb9785f71d0d
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-04-02 21:09:31 -0400

    Hamid added new features now it's ready to be mergedto main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
        person1.name="Hamid";
        person1.lastName="Poya";
        person1.phoneNumber="789-4517";
+
+       Person1Addition add=new Person1Addition();
+       add.name="Rafik";
+       add.phoneNumber="987-5243";
+
     }
 }
