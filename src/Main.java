ce5dc9369118c9acab0e31ffb5c45244dea33769
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-04-02 20:46:45 -0400

    Task of first person

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       TaskFirstPerson person1=new TaskFirstPerson();
+       person1.name="Hamid";
+       person1.lastName="Poya";
+       person1.phoneNumber="789-4517";
+    }
+}
---
+++ src/TaskFirstPerson.java
@@ -0,0 +1,6 @@
+public class TaskFirstPerson {
+    String name;
+    String lastName;
+    String phoneNumber;
+
+}
